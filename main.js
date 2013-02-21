09d698a2c51d23135b64090d3acfa621d29ff904
vanya <vanokua@gmail.com>
2013-02-21 17:39:45 +0200

    add roundabout

---
+++ main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){   
+        
+        var sudoSlider = $("#slider").sudoSlider({
+            slideCount:4,
+            autoheight: false,
+            autowidth: false,
+            // continuous:true,
+            prevhtml:'<a href="#" class="prevBtn arrow arrow-left-active"></a>', /* option[32]/*prevhtml*/
+            nexthtml:'<a href="#" class="nextBtn arrow arrow-right-active"></a>', /* option[33]/*nexthtml*/
+            
+
+        });
+
+
+        var sudoSlider2 = $("#slider2").sudoSlider({
+            slideCount:5,
+            vertical: true,
+            controlsattr: 'id="controls2"',
+            prevhtml:'<a href="#" class="prevBtn2 arrow arrow-up-active"></a>', /* option[32]/*prevhtml*/
+            nexthtml:'<a href="#" class="nextBtn2 arrow arrow-down-active"></a>', /* option[33]/*nexthtml*/
+            
+
+        });
+        
+    });
